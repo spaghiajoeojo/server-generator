5679be1af6e4f7d6d3cde76a4e529062fcd323fe
Beccaro <Lorenzo.Beccaro@konicaminolta.it>
2023-04-27 22:45:00 +0200

    - changed message

--- src/logo.ts
+++ src/logo.ts
@@ -1,31 +1,18 @@
 export const printLogo = async () => {
   const chalk = (await import('chalk')).default;
-  const bluish = chalk.hex('#88c0bf');
-  const reddish = chalk.hex('#fe8e9c');
-  const pastaLight = chalk.hex('#f8d56d');
-  const pastaDark = chalk.hex('#e3c565');
-  const plateLight = chalk.hex('#c7c7c7');
-  const plateDark = chalk.hex('#a8a8a8');
-  console.log(
-    reddish('Hi! You are using a generator powered by ') + bluish('spaghiajoeojo') + reddish('!'),
-    `
-                   ${bluish('###')}
-           ${bluish('###################')}
-       ${bluish('###########################')}
-     ${bluish('##########################')}${reddish('%')}${bluish('####')}
-   ${bluish('##########################')}${reddish('%%')}${bluish('#######')}
-  ${bluish('#########################')}${reddish('%%')}${bluish('#########')}
- ${bluish('#######################')}${pastaLight('%')}${reddish('%%')}${bluish('##############')}
- ${bluish('####################')}${pastaLight('%%')}${pastaDark('###')}${bluish('##############')}
-${bluish('############')}${pastaLight('%%%%%%%%%%%%%%%%')}${bluish('############')}
- ${bluish('########')}${plateDark('#')}${pastaLight('%%')}${plateDark('####################')}${bluish('########')}
- ${bluish('#########')}${pastaLight('%%')}${bluish('/')}${plateLight('#%%%%%%%%%%%%%')}${bluish('///////######')}
-  ${bluish('#########/////////////////////////###')}
-   ${bluish('##########/////////////////////////')}
-     ${bluish('##########/////////////////////')}
-       ${bluish('#########//////////////////')}
-           ${bluish('#######////////////')}
+  const green = chalk.hex('#9ae991');
 
-`,
+  console.log(
+    'Hi! You are using a generator powered by ',
+    green(`
+  ______                                              ____
+ |  ____|                                            |  _ \\
+ | |__    __  __  _ __    _ __    ___   ___   ___    | |_) |   ___    __ _   _ __    ___
+ |  __|   \\ \\/ / | '_ \\  | '__|  / _ \\ / __| / __|   |  _ <   / _ \\  / _\` | | '_ \\  / __|
+ | |____   >  <  | |_) | | |    |  __/ \\__ \\ \\__ \\   | |_) | |  __/ | (_| | | | | | \\__ \\
+ |______| /_/\\_\\ | .__/  |_|     \\___| |___/ |___/   |____/   \\___|  \\__,_| |_| |_| |___/
+                 | |
+                 |_|
+`),
   );
 };
